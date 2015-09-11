72c51e35cb824b3cef78109f50cfec0a52a9687a
Abdul Rahimi <afr114@gmail.com>
2015-09-11 14:11:12 -0700

    add front end styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,17 @@
 var findAndReplace = function(inputString, findString, replaceString) {
-  return inputString.replace( new RegExp( findString, "gi" ), replaceString);
+  return inputString.replace(new RegExp( findString, "gi" ), replaceString);
 };
+
+$(document).ready(function() {
+  $("form#findReplace").submit(function(event) {
+
+    var inputString = $("#phrase").val();
+    var findString = $("input#oldWord").val();
+    var replaceString = $("input#newWord").val();
+    var result = findAndReplace(inputString, findString, replaceString);
+
+    $("#replaceWordPhrase").text(result);
+    $("#result").show();
+    event.preventDefault();
+  });
+});
